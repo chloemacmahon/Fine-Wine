bc844c02f58dae8e27d5880842840f2220bcbc62
AndriesBenade <67644109+AndriesBenade@users.noreply.github.com>
2020-09-27 16:35:39 +0200

    Add files via upload

---
+++ FineWineUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FineWineUtil
+{
+
+    public class DBManager
+    {
+
+        public Sql
+
+    }
+
+}
