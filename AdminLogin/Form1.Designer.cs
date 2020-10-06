2c0a2fe241c5e36a8331852b4d21f9deaf2d175b
Andries Benade <benadeandries@gmail.com>
2020-10-06 08:20:09 +0200

    Fixed graphical property

--- AdminLogin/Form1.Designer.cs
+++ AdminLogin/Form1.Designer.cs
@@ -54,17 +54,19 @@
             this.panel1.Controls.Add(this.txtUsername);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(8, 49);
+            this.panel1.Location = new System.Drawing.Point(11, 60);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(308, 139);
+            this.panel1.Size = new System.Drawing.Size(411, 171);
             this.panel1.TabIndex = 0;
             // 
             // chkBoxShowPassword
             // 
             this.chkBoxShowPassword.AutoSize = true;
-            this.chkBoxShowPassword.Location = new System.Drawing.Point(169, 73);
+            this.chkBoxShowPassword.Location = new System.Drawing.Point(225, 90);
+            this.chkBoxShowPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkBoxShowPassword.Name = "chkBoxShowPassword";
-            this.chkBoxShowPassword.Size = new System.Drawing.Size(102, 17);
+            this.chkBoxShowPassword.Size = new System.Drawing.Size(129, 21);
             this.chkBoxShowPassword.TabIndex = 8;
             this.chkBoxShowPassword.Text = "Show Password";
             this.chkBoxShowPassword.UseVisualStyleBackColor = true;
@@ -75,9 +77,10 @@
             this.button2.Font = new System.Drawing.Font("Mongolian Baiti", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ImageIndex = 0;
             this.button2.ImageList = this.imageList1;
-            this.button2.Location = new System.Drawing.Point(161, 98);
+            this.button2.Location = new System.Drawing.Point(215, 121);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 26);
+            this.button2.Size = new System.Drawing.Size(176, 32);
             this.button2.TabIndex = 6;
             this.button2.Text = "Exit";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -98,9 +101,10 @@
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button1.ImageIndex = 1;
             this.button1.ImageList = this.imageList1;
-            this.button1.Location = new System.Drawing.Point(16, 98);
+            this.button1.Location = new System.Drawing.Point(21, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 26);
+            this.button1.Size = new System.Drawing.Size(176, 32);
             this.button1.TabIndex = 5;
             this.button1.Text = "Login";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -108,26 +112,29 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(148, 47);
+            this.txtPassword.Location = new System.Drawing.Point(191, 60);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(123, 20);
+            this.txtPassword.Size = new System.Drawing.Size(163, 22);
             this.txtPassword.TabIndex = 4;
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(148, 11);
+            this.txtUsername.Location = new System.Drawing.Point(191, 17);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(123, 20);
+            this.txtUsername.Size = new System.Drawing.Size(163, 22);
             this.txtUsername.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 15.35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(35, 44);
+            this.label3.Location = new System.Drawing.Point(47, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 23);
+            this.label3.Size = new System.Drawing.Size(126, 28);
             this.label3.TabIndex = 2;
             this.label3.Text = "Password :";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -136,9 +143,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 15.35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(32, 9);
+            this.label2.Location = new System.Drawing.Point(43, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 23);
+            this.label2.Size = new System.Drawing.Size(132, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Username :";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -147,9 +155,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 20.35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(151, 13);
+            this.label1.Location = new System.Drawing.Point(201, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 30);
+            this.label1.Size = new System.Drawing.Size(96, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -157,22 +166,24 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = global::AdminLogin.Properties.Resources.padlock;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(100, 3);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(133, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox1.Size = new System.Drawing.Size(60, 49);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // AdminLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 196);
+            this.ClientSize = new System.Drawing.Size(432, 241);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdminLogin";
             this.Text = "Administrator Login";
             this.panel1.ResumeLayout(false);
